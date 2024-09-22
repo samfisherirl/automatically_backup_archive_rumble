402508d68f51df7c043ffc72cf1f5607a7d3647a
Sam A <98753696+samfisherirl@users.noreply.github.com>
2024-09-22 17:41:55 -0400

    Add files via upload

--- setup.bat
+++ setup.bat
@@ -1,3 +1,3 @@
 CALL venv\Scripts\activate
 
-venv\Scripts\pyinstaller --console --icon="app.ico" --add-data="venv\Lib\site-packages\Selenium;Selenium" video_handler.py
+venv\Scripts\pyinstaller --noconfirm --console --icon="app.ico" --add-data="venv\Lib\site-packages\Selenium;Selenium" rumble_video_archive.py
